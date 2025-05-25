e1bfab959e9a5d214245bdfbc83a4dca1db4ec21
MananJain2464 <149476364+MananJain2464@users.noreply.github.com>
2025-05-26 00:02:03 +0530

    Updated3

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,34 +1,13 @@
 // app/components/helper/animation-lottie.jsx
-// "use client"; // Keep this directive
+"use client"; // Keep this directive
 
-// import dynamic from 'next/dynamic'; // Import dynamic
+import dynamic from 'next/dynamic'; // Import dynamic
 
-// // Dynamically import Lottie and ensure it's not included in SSR
-// const Lottie = dynamic(
-//   () => import('lottie-react'),
-//   { ssr: false } // This is the key change!
-// );
-
-// const AnimationLottie = ({ animationPath, width }) => {
-//   const defaultOptions = {
-//     loop: true,
-//     autoplay: true,
-//     animationData: animationPath,
-//     style: {
-//       width: width || '95%', // Use the passed width, or default to '95%'
-//     }
-//   };
-
-//   return (
-//     <Lottie {...defaultOptions} />
-//   );
-// };
-
-// export default AnimationLottie;
-
-"use client"
-
-import Lottie from "lottie-react";
+// Dynamically import Lottie and ensure it's not included in SSR
+const Lottie = dynamic(
+  () => import('lottie-react'),
+  { ssr: false } // This is the key change!
+);
 
 const AnimationLottie = ({ animationPath, width }) => {
   const defaultOptions = {
@@ -36,7 +15,7 @@ const AnimationLottie = ({ animationPath, width }) => {
     autoplay: true,
     animationData: animationPath,
     style: {
-      width: '95%',
+      width: width || '95%', // Use the passed width, or default to '95%'
     }
   };
 
@@ -46,3 +25,24 @@ const AnimationLottie = ({ animationPath, width }) => {
 };
 
 export default AnimationLottie;
+
+// "use client"
+
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
+
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
